19daee27f4c7f5b4f21ffe66d9ffb5ef7b52eb7d
aldo2102 <aldoh.ti@gmail.com>
2015-11-26 12:23:59 -0200

    primeiro

---
+++ WebFormMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormMVC.Startup))]
+namespace WebFormMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
